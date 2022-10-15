2adef6bb30d291df7e061c708537d05006af437d
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-10-15 18:16:55 +0500

    fix: missing class scope

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,16 +19,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direction = true) {
+    this.direction = direction;
+    this.alphabet = 'abcdefghijklmnopqrstuvwxyz';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    return this.getMachineResult(message, key, true);
+  }
+  decrypt(message, key) {
+    return this.getMachineResult(message, key, false);
+  }
+  createKeyStr(key, length) {
+    return key.repeat(Math.ceil(length / key.length)).slice(0, length);
+  }
+  getIndexes(messageChar, keyChar) {
+    return [this.getIndex(messageChar), this.getIndex(keyChar)];
+  }
+  getIndex(ch) {
+    return this.alphabet.indexOf(ch.toLowerCase());
+  }
+  getMachineResult(message, key, ecnryptFlag) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    const keyStr = this.createKeyStr(key, message.length);
+    let skipCount = 0;
+    const result = message.split('').map((c, i) => {
+      const [messageIndex, keyIndex] = this.getIndexes(c, keyStr[i - skipCount]);
+      if (messageIndex === -1) {
+        skipCount += 1;
+        return c;
+      }
+      return this.alphabet[
+        ecnryptFlag
+          ? (messageIndex + keyIndex) % 26
+          : messageIndex > keyIndex
+          ? messageIndex - keyIndex
+          : (messageIndex - keyIndex + 26) % 26
+      ];
+    });
+    return (this.direction ? result.join('') : result.reverse().join('')).toUpperCase();
   }
 }
+
+
 
 module.exports = {
   VigenereCipheringMachine
