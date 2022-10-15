3bb4ef2529edd7432bbddf02033475006641a755
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-10-15 13:27:50 +0500

    feat:the solution of the first exercise is solve

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity) {
+  if (
+    typeof sampleActivity !== 'string' ||
+    !/^[-+]?[0-9]*[.]?[0-9]+$/.test(sampleActivity)
+  )
+    return false;
+  let value = Number(sampleActivity);
+  if (value <= 0 || value > 15) return false;
+  let num = Math.ceil(
+    Math.log(MODERN_ACTIVITY / value) / (Math.log(2) / HALF_LIFE_PERIOD)
+  );
+  return num;
 }
 
 module.exports = {
