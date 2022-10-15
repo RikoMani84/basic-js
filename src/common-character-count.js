4affb8c800df24a87436674508223d9c1f243121
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-10-15 16:41:30 +0500

    Revert "feat:he solution of the second exercise is solve"
    This reverts commit 565a45ff6e4b28919d5f20b2e561bd8ae31467bf.

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,17 +11,8 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount( s1, s2 ) {
-  let count = 0;
-  let str = s2;
-  for (let i = 0; i < s1.length; i++) {
-    const index = str.indexOf(s1[i]);
-    if (index !== -1) {
-      str = str.substr(0, index) + str.substr(index + 1);
-      count++;
-    }
-  }
-  return count;
+function getCommonCharacterCount(/* s1, s2 */) {
+  throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
