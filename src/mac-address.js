0763b89b39cfce08ee687880cbb249db39087206
Rikki Monti <110960926+RikoMani84@users.noreply.github.com>
2022-10-15 18:05:50 +0500

    feat: answers to mac-address and matrix-elements-sum tasks

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address( n ) {
+  let nArr = n.split('-')
+  let arr = []
+  if (nArr.length === 6) {
+   for (let i = 0; i < nArr.length; i++) {
+     arr.push(nArr[i].split(''))
+   }
+   for (let i = 0; i < arr.length; i++) {
+     if (isNaN(Number(arr[i][0])) && ('A' <= arr[i][0] && arr[i][0] <= 'F') || !isNaN(Number(arr[i][0])) && 0 <= Number(arr[i][0]) <= 9) {
+       if (isNaN(Number(arr[i][1])) && ('A' <= arr[i][1] && arr[i][1] <= 'F') || !isNaN(Number(arr[i][1])) && 0 <= Number(arr[i][1]) <= 9) {
+         return true
+       }
+       else return false
+     }
+     else return false
+   }
+  }
+  return false
 }
 module.exports = {
   isMAC48Address
